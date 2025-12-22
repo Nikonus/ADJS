45f978b0061219b16af0bd655d339464133bb6ff
Nikonus <nikhildubay.2005@gmail.com>
2025-12-22 21:11:21 +0530

    added

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,20 @@
-Object.setPrototypeOf(teacherSupport , user)
+//class user{
+//     constructor(name, email , password){
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//     }
+//     encriptedpassword(){
+//         return `the encripted password is @${this.password}****`
+//     }
+
+//     changeusername(){
+//         return`${this.name.toUpperCase()}`
+//     }   
+// }
+
+
+// const nikhil = new user("nikhil" ,"nikh@.com", "1234");
+// console.log(nikhil);
+// console.log(nikhil.encriptedpassword());
+// console.log(nikhil.changeusername());
